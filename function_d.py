0af7fe17269cb07507989ceee34bb06edc1824a2
Ann <anntuminello@gmail.com>
2023-03-25 06:54:50 -0700

    This is a fix to an infinite loop

--- function_d.py
+++ function_d.py
@@ -1,6 +1,11 @@
 def max_value(numbers):
     
-    largest_number = max_value(numbers)
+    max_value = numbers[0]
+    for i in numbers:
+        if i > max_value:
+            max_value = i
+    return max_value
+
 
 
 
